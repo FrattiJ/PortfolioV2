61b1fe1e4e552beecb712fe25f247efadac3778a
Jacob Fratti <JacobRFratti@gmail.com>
2024-08-31 09:57:57 -0400

    Added simple nav bar for ease of route testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import ClientProvider from '../components/ClientProvider';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'Jacob Fratti',
@@ -10,7 +11,37 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <nav>
+          <ul className='flex flex-wrap justify-evenly align-center'>
+            <li>
+              <Link href="/" passHref>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/resume" passHref>
+                Resume
+              </Link>
+            </li>
+            <li>
+              <Link href="/tech-blog" passHref>
+                Tech Blog
+              </Link>
+            </li>
+            <li>
+              <Link href="/wood-blog" passHref>
+                Woodworking Blog
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" passHref>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
         <ClientProvider>{children}</ClientProvider>
+        {/* Maybe add a volleyball blog as well with the ability to post highlight clips */}
       </body>
     </html>
   );
